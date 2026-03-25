5aa4672452ad394f74c09a393cbd38c3aa85ac85
Lakshmikanth m <lakshmikanthjagan@gmail.com>
2026-03-25 18:52:09 +0530

    Initial DevOps app

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head><title>DevOps Demo</title></head>
+      <body style="font-family: Arial; text-align: center; padding: 50px;">
+        <h1>🚀 DevOps Pipeline Demo</h1>
+        <p>Version: 1.0.0</p>
+        <p>Built with Jenkins | Stored in Nexus | Deployed on Kubernetes</p>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
