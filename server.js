d595c5e0e24655b2bcad06ef47d4f68aa08ef3bc
Lakshmikanth m <lakshmikanthjagan@gmail.com>
2026-03-31 16:42:51 +0530

    updated server.js 1 file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send(`
     <html>
-      <head><title>DevOps Demo</title></head>
+      <head><title>DevOps Demo with docker kubernetes and </title></head>
       <body style="font-family: Arial; text-align: center; padding: 50px;">
         <h1>🚀 DevOps Pipeline Demo</h1>
         <p>Version: 1.0.67</p>
