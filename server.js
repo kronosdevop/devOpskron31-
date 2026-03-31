f4fc8c6d8a929e082923d3dfb00a7296fb41f8ae
Lakshmikanth m <lakshmikanthjagan@gmail.com>
2026-03-31 16:39:34 +0530

    new server.js 1 file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
       <head><title>DevOps Demo</title></head>
       <body style="font-family: Arial; text-align: center; padding: 50px;">
         <h1>🚀 DevOps Pipeline Demo</h1>
-        <p>Version: 1.0.0</p>
+        <p>Version: 1.0.67</p>
         <p>Built with Jenkins | Stored in Nexus | Deployed on Kubernetes</p>
         <p>Project built by Lakshmikanth</p>
       </body>
