588c9515160eb112b9b80eac695ebeff33953bfa
Lakshmikanth m <lakshmikanthjagan@gmail.com>
2026-05-21 14:43:50 +0530

    wall labs project

--- public/src/utils/google-maps.js
+++
@@ -1,9 +0,0 @@
-import { Loader } from "@googlemaps/js-api-loader";
-
-const loader = new Loader({
-  apiKey: "AIzaSyCiwPvMBsTQy6yiTJDvDkqZPIWdDerOcRI", 
-  version: "weekly",
-  libraries: ["places"], // Load all necessary libraries at once
-});
-
-export const googleApi = loader.load(); 
